07c969d1d0690deee2d26c1df21d3b52ee2b50c1
50612087017 <50612087017@KLASSA302-27>
2024-10-18 10:07:36 +0300

    Added DbContext (DbSet)

--- KooliProjekt/Data/ApplicationDbContext.cs
+++ KooliProjekt/Data/ApplicationDbContext.cs
@@ -9,5 +9,11 @@ namespace KooliProjekt.Data
             : base(options)
         {
         }
+        public DbSet<Patient> Patient { get; set; }
+        public DbSet<Worker> Worker { get; set; }
+        public DbSet<Health_data> Health_data { get; set; }
+        public DbSet<Quantity> Quantity { get; set; }
+        public DbSet<Nutrients> Nutrients { get; set; }
+
     }
 }
--- KooliProjekt/Data/Patient.cs
+++ KooliProjekt/Data/Patient.cs
@@ -6,8 +6,8 @@
 
         public string Name { get; set; }
 
-   //     public string Health_data { get; set; }
+        //     public Health_data Health_data { get; set; }
 
-   //     public string Nutrition { get; set; }
+        //     public Nutrition Nutrition { get; set; }
     }
 }
