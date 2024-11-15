ff04c239384e1eb69ff6f336ec1aa313d050f4f6
8Tauri8 <Tauri.Tammela@tptlive.ee>
2024-11-15 09:28:52 +0200

    Program.cs ....

---
+++ KooliProjekt/Data/SeedData.cs
@@ -0,0 +1,17 @@
+﻿using KooliProjekt.Data;
+
+
+
+    public static class SeedData
+    {
+        // SeedData genereerimise meetod
+        public static void Generate(ApplicationDbContext dbContext)
+        {
+            if (dbContext.Users.Any())
+            {
+                return;
+            }
+
+            dbContext.SaveChanges(); // Salvestame muudatused
+        }
+    }
