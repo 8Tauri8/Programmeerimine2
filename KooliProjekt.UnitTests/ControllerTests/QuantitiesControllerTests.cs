9397010a1360845e8743ea869efe02e231211028
Tauri <Tauri.Tammela@tptlive.ee>
2025-01-24 09:11:28 +0200

    QuantitiesControllerTests Create,Edit,Delete .. work

--- KooliProjekt.UnitTests/ControllerTests/QuantitiesControllerTests.cs
+++ KooliProjekt.UnitTests/ControllerTests/QuantitiesControllerTests.cs
@@ -1,46 +1,226 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using KooliProjekt.Controllers;
+﻿using KooliProjekt.Controllers;
 using KooliProjekt.Data;
 using KooliProjekt.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Moq;
 using Xunit;
 
 namespace KooliProjekt.UnitTests.ControllerTests
 {
-    public class QuanityControllerTests
+    public class QuantitiesControllerTests
     {
         private readonly Mock<IQuantityService> _QuantityServiceMock;
         private readonly QuantitiesController _controller;
 
-        public QuanityControllerTests()
+        public QuantitiesControllerTests()
         {
             _QuantityServiceMock = new Mock<IQuantityService>();
             _controller = new QuantitiesController(_QuantityServiceMock.Object);
         }
+
         [Fact]
-        public async Task Index_should_return_correct_view_with_data()
+        public async Task Index_should_return_view_and_data()
         {
             // Arrange
-            int page = 1;
+            var page = 1;
             var data = new List<Quantity>
             {
                 new Quantity { id = 1, Nutrients = 23, Amount = 2},
                 new Quantity { id = 2, Nutrients = 33, Amount = 6},
             };
-            var pagedResult = new PagedResult<Quantity> { Results = data };
-            _QuantityServiceMock.Setup(x => x.List(page, It.IsAny<int>())).ReturnsAsync(pagedResult);
+            var pagedResult = new PagedResult<Quantity>
+            {
+                Results = data,
+                CurrentPage = 1,
+                PageCount = 1,
+                PageSize = 5,
+                RowCount = 2
+            };
+            _QuantityServiceMock
+                .Setup(x => x.List(page, It.IsAny<int>()))
+                .ReturnsAsync(pagedResult);
 
             // Act
             var result = await _controller.Index(page) as ViewResult;
 
             // Assert
             Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Index"
+            );
             Assert.Equal(pagedResult, result.Model);
+        }
+
+        [Fact]
+        public async Task Details_should_return_notfound_when_id_is_missing()
+        {
+            // Arrange
+            int? id = null;
+
+            // Act
+            var result = await _controller.Details(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Details_should_return_notfound_when_list_is_missing()
+        {
+            // Arrange
+            int id = 1;
+            var list = (Quantity)null;
+            _QuantityServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Details(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Details_should_return_view_with_model_when_list_was_found()
+        {
+            // Arrange
+            int id = 1;
+            var list = new Quantity { id = id };
+            _QuantityServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Details(id) as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Details"
+            );
+            Assert.Equal(list, result.Model);
+        }
+
+        [Fact]
+        public void Create_should_return_view()
+        {
+            // Act
+            var result = _controller.Create() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Create"
+            );
+        }
+
+        [Fact]
+        public async Task Edit_should_return_notfound_when_id_is_missing()
+        {
+            // Arrange
+            int? id = null;
+
+            // Act
+            var result = await _controller.Edit(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Edit_should_return_notfound_when_list_is_missing()
+        {
+            // Arrange
+            int id = 1;
+            var list = (Quantity)null;
+            _QuantityServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Edit(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Edit_should_return_view_with_model_when_list_was_found()
+        {
+            // Arrange
+            int id = 1;
+            var list = new Quantity { id = id };
+            _QuantityServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Edit(id) as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Edit"
+            );
+            Assert.Equal(list, result.Model);
+        }
+
+        [Fact]
+        public async Task Delete_should_return_notfound_when_id_is_missing()
+        {
+            // Arrange
+            int? id = null;
+
+            // Act
+            var result = await _controller.Delete(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Delete_should_return_notfound_when_list_is_missing()
+        {
+            // Arrange
+            int id = 1;
+            var list = (Quantity)null;
+            _QuantityServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Delete(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Delete_should_return_view_with_model_when_list_was_found()
+        {
+            // Arrange
+            int id = 1;
+            var list = new Quantity { id = id };
+            _QuantityServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Delete(id) as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Delete"
+            );
+            Assert.Equal(list, result.Model);
         }
     }
 }
