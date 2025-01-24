88ea6a47dc2a7ab8e6c519c14a3c04e9377a708b
Tauri <Tauri.Tammela@tptlive.ee>
2025-01-24 09:08:55 +0200

    PatientControllerTests Create,Edit,Delete .. work

--- KooliProjekt.UnitTests/ControllerTests/PatientsControllerTests.cs
+++ KooliProjekt.UnitTests/ControllerTests/PatientsControllerTests.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using KooliProjekt.Controllers;
+﻿using KooliProjekt.Controllers;
 using KooliProjekt.Data;
 using KooliProjekt.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Moq;
 using Xunit;
 
@@ -14,33 +10,217 @@ namespace KooliProjekt.UnitTests.ControllerTests
 {
     public class PatientsControllerTests
     {
-        private readonly Mock<IPatientService> _PatientsServiceMock;
+        private readonly Mock<IPatientService> _PatientServiceMock;
         private readonly PatientsController _controller;
 
         public PatientsControllerTests()
         {
-            _PatientsServiceMock = new Mock<IPatientService>();
-            _controller = new PatientsController(_PatientsServiceMock.Object);
+            _PatientServiceMock = new Mock<IPatientService>();
+            _controller = new PatientsController(_PatientServiceMock.Object);
         }
+
         [Fact]
-        public async Task Index_should_return_correct_view_with_data()
+        public async Task Index_should_return_view_and_data()
         {
             // Arrange
-            int page = 1;
+            var page = 1;
             var data = new List<Patient>
             {
                 new Patient { id = 1, Name = "Kutsu", HealthData = "Good", Nutrition = "Stable"},
                 new Patient { id = 2, Name = "Asvu", HealthData = "Bad", Nutrition = "Bad"},
             };
-            var pagedResult = new PagedResult<Patient> { Results = data };
-            _PatientsServiceMock.Setup(x => x.List(page, It.IsAny<int>())).ReturnsAsync(pagedResult);
+            var pagedResult = new PagedResult<Patient>
+            {
+                Results = data,
+                CurrentPage = 1,
+                PageCount = 1,
+                PageSize = 5,
+                RowCount = 2
+            };
+            _PatientServiceMock
+                .Setup(x => x.List(page, It.IsAny<int>()))
+                .ReturnsAsync(pagedResult);
 
             // Act
             var result = await _controller.Index(page) as ViewResult;
 
             // Assert
             Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Index"
+            );
             Assert.Equal(pagedResult, result.Model);
+        }
+
+        [Fact]
+        public async Task Details_should_return_notfound_when_id_is_missing()
+        {
+            // Arrange
+            int? id = null;
+
+            // Act
+            var result = await _controller.Details(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Details_should_return_notfound_when_list_is_missing()
+        {
+            // Arrange
+            int id = 1;
+            var list = (Patient)null;
+            _PatientServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Details(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Details_should_return_view_with_model_when_list_was_found()
+        {
+            // Arrange
+            int id = 1;
+            var list = new Patient { id = id };
+            _PatientServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Details(id) as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Details"
+            );
+            Assert.Equal(list, result.Model);
+        }
+
+        [Fact]
+        public void Create_should_return_view()
+        {
+            // Act
+            var result = _controller.Create() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Create"
+            );
+        }
+
+        [Fact]
+        public async Task Edit_should_return_notfound_when_id_is_missing()
+        {
+            // Arrange
+            int? id = null;
+
+            // Act
+            var result = await _controller.Edit(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Edit_should_return_notfound_when_list_is_missing()
+        {
+            // Arrange
+            int id = 1;
+            var list = (Patient)null;
+            _PatientServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Edit(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Edit_should_return_view_with_model_when_list_was_found()
+        {
+            // Arrange
+            int id = 1;
+            var list = new Patient { id = id };
+            _PatientServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Edit(id) as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Edit"
+            );
+            Assert.Equal(list, result.Model);
+        }
+
+        [Fact]
+        public async Task Delete_should_return_notfound_when_id_is_missing()
+        {
+            // Arrange
+            int? id = null;
+
+            // Act
+            var result = await _controller.Delete(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Delete_should_return_notfound_when_list_is_missing()
+        {
+            // Arrange
+            int id = 1;
+            var list = (Patient)null;
+            _PatientServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Delete(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Delete_should_return_view_with_model_when_list_was_found()
+        {
+            // Arrange
+            int id = 1;
+            var list = new Patient { id = id };
+            _PatientServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Delete(id) as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Delete"
+            );
+            Assert.Equal(list, result.Model);
         }
     }
 }
