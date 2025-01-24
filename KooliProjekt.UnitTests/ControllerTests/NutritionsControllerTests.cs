183c821617c7ef2199506893d205826619ecf712
Tauri <Tauri.Tammela@tptlive.ee>
2025-01-24 09:05:45 +0200

    NutritionControllerTests Create,Edit,Delete .. work

--- KooliProjekt.UnitTests/ControllerTests/NutritionsControllerTests.cs
+++ KooliProjekt.UnitTests/ControllerTests/NutritionsControllerTests.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using KooliProjekt.Controllers;
+﻿using KooliProjekt.Controllers;
 using KooliProjekt.Data;
 using KooliProjekt.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Moq;
 using Xunit;
 
@@ -14,34 +10,218 @@ namespace KooliProjekt.UnitTests.ControllerTests
 {
     public class NutritionsControllerTests
     {
-        private readonly Mock<INutritionService> _NutritionsServiceMock;
+        private readonly Mock<INutritionService> _NutritionServiceMock;
         private readonly NutritionsController _controller;
 
         public NutritionsControllerTests()
         {
-            _NutritionsServiceMock = new Mock<INutritionService>();
-            _controller = new NutritionsController(_NutritionsServiceMock.Object);
+            _NutritionServiceMock = new Mock<INutritionService>();
+            _controller = new NutritionsController(_NutritionServiceMock.Object);
         }
+
         [Fact]
-        public async Task Index_should_return_correct_view_with_data()
+        public async Task Index_should_return_view_and_data()
         {
             // Arrange
-            int page = 1;
+            var page = 1;
             var data = new List<Nutrition>
             {
                 new Nutrition { id = 1, Eating_time = new DateTime(2019,05,09), Nutrients = 34, Quantity = 12},
                 new Nutrition { id = 2, Eating_time = new DateTime(2025,12,10), Nutrients = 23, Quantity = 32},
 
             };
-            var pagedResult = new PagedResult<Nutrition> { Results = data };
-            _NutritionsServiceMock.Setup(x => x.List(page, It.IsAny<int>())).ReturnsAsync(pagedResult);
+            var pagedResult = new PagedResult<Nutrition>
+            {
+                Results = data,
+                CurrentPage = 1,
+                PageCount = 1,
+                PageSize = 5,
+                RowCount = 2
+            };
+            _NutritionServiceMock
+                .Setup(x => x.List(page, It.IsAny<int>()))
+                .ReturnsAsync(pagedResult);
 
             // Act
             var result = await _controller.Index(page) as ViewResult;
 
             // Assert
             Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Index"
+            );
             Assert.Equal(pagedResult, result.Model);
+        }
+
+        [Fact]
+        public async Task Details_should_return_notfound_when_id_is_missing()
+        {
+            // Arrange
+            int? id = null;
+
+            // Act
+            var result = await _controller.Details(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Details_should_return_notfound_when_list_is_missing()
+        {
+            // Arrange
+            int id = 1;
+            var list = (Nutrition)null;
+            _NutritionServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Details(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Details_should_return_view_with_model_when_list_was_found()
+        {
+            // Arrange
+            int id = 1;
+            var list = new Nutrition { id = id };
+            _NutritionServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Details(id) as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Details"
+            );
+            Assert.Equal(list, result.Model);
+        }
+
+        [Fact]
+        public void Create_should_return_view()
+        {
+            // Act
+            var result = _controller.Create() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Create"
+            );
+        }
+
+        [Fact]
+        public async Task Edit_should_return_notfound_when_id_is_missing()
+        {
+            // Arrange
+            int? id = null;
+
+            // Act
+            var result = await _controller.Edit(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Edit_should_return_notfound_when_list_is_missing()
+        {
+            // Arrange
+            int id = 1;
+            var list = (Nutrition)null;
+            _NutritionServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Edit(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Edit_should_return_view_with_model_when_list_was_found()
+        {
+            // Arrange
+            int id = 1;
+            var list = new Nutrition { id = id };
+            _NutritionServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Edit(id) as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Edit"
+            );
+            Assert.Equal(list, result.Model);
+        }
+
+        [Fact]
+        public async Task Delete_should_return_notfound_when_id_is_missing()
+        {
+            // Arrange
+            int? id = null;
+
+            // Act
+            var result = await _controller.Delete(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Delete_should_return_notfound_when_list_is_missing()
+        {
+            // Arrange
+            int id = 1;
+            var list = (Nutrition)null;
+            _NutritionServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Delete(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task Delete_should_return_view_with_model_when_list_was_found()
+        {
+            // Arrange
+            int id = 1;
+            var list = new Nutrition { id = id };
+            _NutritionServiceMock
+                .Setup(x => x.Get(id))
+                .ReturnsAsync(list);
+
+            // Act
+            var result = await _controller.Delete(id) as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(
+                string.IsNullOrEmpty(result.ViewName) ||
+                result.ViewName == "Delete"
+            );
+            Assert.Equal(list, result.Model);
         }
     }
 }
