cd8b5620fd1bd6ed655a5adb507f5d231a0dee33
Tauri <Tauri.Tammela@tptlive.ee>
2025-02-28 09:08:29 +0200

    HealthDatas search works again

--- KooliProjekt/Controllers/HealthDatasController.cs
+++ KooliProjekt/Controllers/HealthDatasController.cs
@@ -2,67 +2,60 @@
 using KooliProjekt.Models;
 using KooliProjekt.Services;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Policy;
 using System.Threading.Tasks;
-
+using KooliProjekt.Search; // Make sure to include this
 namespace KooliProjekt.Controllers
 {
     public class HealthDatasController : Controller
     {
         private readonly IHealthDataService _HealthDataService;
-
         public HealthDatasController(IHealthDataService HealthDataService)
         {
             _HealthDataService = HealthDataService;
         }
-
-        public async Task<IActionResult> Index(int page = 1)
+        // GET: HealthDatas
+        public async Task<IActionResult> Index(int page = 1, HealthDatasIndexModel model = null)
         {
+            model = model ?? new HealthDatasIndexModel();
             int pageSize = 5;
-            return View(await _HealthDataService.List(page, pageSize));
+            // Assuming you have a List method in your service that accepts search parameters
+            model.Data = await _HealthDataService.List(page, pageSize, model.Search);
+            return View(model);
         }
-
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-
             var healthData = await _HealthDataService.Get(id.Value);
             if (healthData == null)
             {
                 return NotFound();
             }
-
             return View(healthData);
         }
-
         public IActionResult Create()
         {
             return View();
         }
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("id,Weight,Blood_pressure,Blood_sugar")] HealthData healthData)
         {
             if (ModelState.IsValid)
             {
-                await _HealthDataService.Save(healthData);  
+                await _HealthDataService.Save(healthData);
                 return RedirectToAction(nameof(Index));
             }
             return View(healthData);
         }
-
-
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-
             var healthData = await _HealthDataService.Get(id.Value);
             if (healthData == null)
             {
@@ -70,8 +63,6 @@ namespace KooliProjekt.Controllers
             }
             return View(healthData);
         }
-
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("id,Weight,Blood_pressure,Blood_sugar")] HealthData healthData)
@@ -80,37 +71,31 @@ namespace KooliProjekt.Controllers
             {
                 return NotFound();
             }
-
             if (ModelState.IsValid)
             {
-                await _HealthDataService.Save(healthData);  
+                await _HealthDataService.Save(healthData);
                 return RedirectToAction(nameof(Index));
             }
             return View(healthData);
         }
-
-
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-
             var healthData = await _HealthDataService.Get(id.Value);
             if (healthData == null)
             {
                 return NotFound();
             }
-
             return View(healthData);
         }
-
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            await _HealthDataService.Delete(id); 
+            await _HealthDataService.Delete(id);
             return RedirectToAction(nameof(Index));
         }
     }
